d0eee8a55cd276d7c38760c121f0c429d1a16467
Baharuddin Kasim <kasim.baharuddin@gmail.com>
2022-07-04 16:36:42 +0800

    update koneksi

--- public/generateSurat/koneksi.php
+++ public/generateSurat/koneksi.php
@@ -1,10 +1,13 @@
-<?php 
-    //koneksi ke php
-    $host = "127.0.0.1";
-    $user = "root";
-    $pass = "";
-    $dbnm = "coba_database";
+<?php
+//koneksi ke db
+// $host = "127.0.0.1";
+// $user = "root";
+// $pass = "";
+// $dbnm = "coba_database";
 
-    $connect = mysqli_connect($host, $user, $pass, $dbnm);
+$host = "103.195.142.83";
+$user = "desalampenai";
+$pass = "@Lampenai2022";
+$dbnm = "desa_lampenai_testing";
 
-?>
+$connect = mysqli_connect($host, $user, $pass, $dbnm);
